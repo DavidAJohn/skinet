52780daaa166dcafd8a58ec29b904b27d6aef906
David John <davidajohn@gmail.com>
2020-07-17 15:28:10 +0100

    Added CORS policy to Startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,10 +26,15 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            
             services.AddApplicationServices(); // custom services extension
-
             services.AddSwaggerServiceOptions(); // custom services extension
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +49,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
