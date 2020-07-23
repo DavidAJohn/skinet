60300562b809f9b5aa7c4dcd093d55aadf358e1c
David John <davidajohn@gmail.com>
2020-07-23 17:54:37 +0100

    Fixed bug when pager and filter used together

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -61,11 +61,13 @@ export class ShopComponent implements OnInit  {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -75,12 +77,15 @@ export class ShopComponent implements OnInit  {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
