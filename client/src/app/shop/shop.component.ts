a35a3b6f69edb28dd3efbd3e46b6feb4a636be25
David John <davidajohn@gmail.com>
2020-07-23 16:50:16 +0100

    Refactored pagination into separate components

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -74,7 +74,7 @@ export class ShopComponent implements OnInit  {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event.page;
+    this.shopParams.pageNumber = event;
     this.getProducts();
   }
 }
